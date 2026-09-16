ee8785a3beaf769136847f64c4400cb291f50ecd
victorouma7 <oumaassoyoh@gmail.com>
2026-09-16 11:26:23 +0300

    feature:create main CLI application entry point with configuration parsing

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"flag"
+	"time"
+	"log"
 	"fmt"
-	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		return
-	}	
+	// Define and configure command-linr flags
+	// Default to 10 second timeout if the flag is ommited
+	timeout := flag.Duration("timeout", 10*time.Second, "HTTP timeout duration")
+	// Parse the flag arguments from os.Args[1:]. must be called before accessing flags
+	flag.Parse()
+	// Grab the remaining non-flag command-line arguments
+	args := flag.Args()
+	// Ensure atleast one argument (the target URL or file path) was porvided
+	if len(args) <1 {
+		log.Fatal("Usage: go run cmd/min.go [options] <URL or local file path>\nExample: go run cmd/main.go https://example.com")
+	}
+	target := args[0]
+	f := fetcher.New(*timeout)
+	s := Scraper.New(f)
+
+	fmt.Printf("Scraping target: %...\n", target)
+	data, err := s.Scrape(target)
 }
