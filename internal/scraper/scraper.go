dcb7d9ad04ca3284c90b24c6faccd490864fc48f
victorouma7 <oumaassoyoh@gmail.com>
2026-09-16 10:41:09 +0300

    add: Scrape orchestrationworkflow to the scraper struct

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -10,3 +10,16 @@ type Scraper struct {
 func New(f *fetcher.Fetcher) *Scraper {
 	return &Scraper{fetcher: f}
 }
+
+func (s *Scraper) Scrape(source string) (*models.PageData, error) {
+	stream, resolvedBase, err := s.Fetcher.FetchSource(source)
+	if err != nil {
+		return nil, err
+	}
+	defer stream.Close()
+
+	title, links, images err := parse.ExtractData(stream, resovedBase)
+	if err != nil { 
+		return nil, fmt.Errorf ("parssing failed: %w", err)
+	}
+}
